0e9ef79eb8f510705ad8e9379238ad39c8e7346d
Sagar Rajendran <vrajendr@uwaterloo.ca>
2025-09-25 22:36:45 -0400

    CoM domain randomization

--- legged_lab/envs/base/base_config.py
+++ legged_lab/envs/base/base_config.py
@@ -210,7 +210,7 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="pelvis"),
-            "com_range": {"x": (-0.075 , 0.075), "y": (-0.075, 0.075), "z": (-0.075, 0.075)},
+            "com_range": {"x": (-0.01 , 0.01), "y": (-0.01, 0.01), "z": (-0.01, 0.01)},
         },
     )
 
